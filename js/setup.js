ffa449807db25183ce6759a160b41df2f6157be5
mayami2027 <mayami@sas.upenn.edu>
2025-04-10 16:45:28 -0400

    Add files via upload

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
   center: [42.054209094075276, -87.67777888295966],
-  zoom: 28,
+  zoom: 18,
 });
 
 var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
