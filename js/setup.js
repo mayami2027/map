72fc1f1b9a0198c729f343634252f25e43b7e4a1
mayami2027 <mayami@sas.upenn.edu>
2025-04-10 16:05:53 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
   center: [42.054209094075276, -87.67777888295966],
-  zoom: 18,
+  zoom: 28,
 });
 
 var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
